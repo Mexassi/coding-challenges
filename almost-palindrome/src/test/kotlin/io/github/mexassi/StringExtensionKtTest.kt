d3f39dd612801a1a61fe816d53c6c90bfba7d03c
Massimo Presta <mexassi85@gmail.com>
2020-06-28 09:25:05 +1000

    adding comments to tests

--- almost-palindrome/src/test/kotlin/io/github/mexassi/StringExtensionKtTest.kt
+++ almost-palindrome/src/test/kotlin/io/github/mexassi/StringExtensionKtTest.kt
@@ -1,11 +1,13 @@
 package io.github.mexassi
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 internal class StringExtensionKtTest {
 
     @Test
+    @DisplayName("Halve odd length string should leave the middle char behind")
     fun halve_odd() {
         val pair = StringExtension.halve("MADAM")
 
@@ -25,9 +27,12 @@ internal class StringExtensionKtTest {
 
     @Test
     fun almostPalindrome() {
+        // those are 1 char away from being palindrome
         assertTrue("MADIM".almostPalindrome())
         assertTrue("1234322".almostPalindrome())
+        // this is 2 chars away from being palindrome
         assertFalse("1234333".almostPalindrome())
+        // the following are all palindrome
         assertFalse("MADAM".almostPalindrome())
         assertFalse("1234321".almostPalindrome())
         assertFalse("ABBA".almostPalindrome())
