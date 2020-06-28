2fb20193ce6c7111f795b24efc65ecb31e0858c2
Massimo Presta <mexassi85@gmail.com>
2020-06-28 14:57:28 +1000

    palindromic anagram file definition

---
+++ palindromic-anagrams/src/main/kotlin/io/github/mexassi/PalindromicAnagram.kt
@@ -0,0 +1,8 @@
+package io.github.mexassi
+
+fun String.isPalindromicAnagram() = palindromicAnagram(this)
+
+private fun palindromicAnagram(string: String): Boolean {
+    // TODO implement logic
+    return true
+}
